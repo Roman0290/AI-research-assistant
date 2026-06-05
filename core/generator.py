c4ec8f0f5ac12ae5a980ab8c65804e72c76f66f7
Roman kebede <romankebede69@gmail.com>
2026-06-05 17:13:15 +0300

    Adding requirement

--- core/generator.py
+++ core/generator.py
@@ -1,6 +1,6 @@
 # LLM and prompt templating logic
-from langchain.prompts import PromptTemplate
 from langchain_groq import ChatGroq
+from langchain_core.prompts import PromptTemplate
 from langchain_core.runnables import RunnablePassthrough, RunnableMap, RunnableLambda
 from langchain_core.output_parsers import StrOutputParser
 from typing import Dict, Any, Optional, List
